d58272c1dd646a9c136a0f195044fd997cce8cc1
Bochan kim <44803830+bo1030@users.noreply.github.com>
2019-12-01 21:50:28 +0900

    update

--- bixby/team2_project.student_convenience/code/LoginEx.js
+++ bixby/team2_project.student_convenience/code/LoginEx.js
@@ -2,20 +2,22 @@ module.exports.function = function loginEx (resultFunction) {
   const console=require('console');
 
   console.log(resultFunction);
-
+  var base64 = require('base64');
   var http = require('http');
+  var id  = base64.encode("qhcks1030");
+  var pass = base64.encode("Qhhcks0525104!");
   var options = {
-    format: 'json',
-    headers: {
-      'accept': 'application/x-www-form-urlencoded'
-    }
-  }
-  var pal={
-    "username": "",
-    "password": ""
-  }
+    format : "json",
+    headers:{
+      "Authorization": "Basic " + base64.encode("qhcks1030" + ":" + "Qhcks0525104!"),
+      "X-Requested-With": "XMLHttpRequest"
+    },
+    returnHeaders:true
+  };
   
-  var response = http.postUrl(resultFunction.url, pal, options);
+  var response = http.getUrl(resultFunction.url, options);
+
+  console.log(response);
 
   return response;
 }
